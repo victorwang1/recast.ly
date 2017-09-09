37a50ea1f178dae815d1f3de65a8fdee44997dab
victorwang1 <victor.wang@me.com>
2017-09-09 15:54:34 -0700

    views loading properly

--- src/components/VideoDetails.jsx
+++ src/components/VideoDetails.jsx
@@ -1,38 +1,14 @@
 class VideoDetails extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      views: ''
-    };
-    this.request((view) => this.setState({views: view}));
-  }
-  request(callback) {
-    $.ajax({
-      type: 'GET',
-      data: {
-        id: this.props.videoId,
-        part: 'statistics',
-        key: YOUTUBE_API_KEY
-      },
-      url: 'https://www.googleapis.com/youtube/v3/videos',
-      success: function(data) {
-        console.log(data);
-        callback(data.items[0].statistics.viewCount);
-      },
-      error: function(data) {
-        console.log('Did not get statistics' + data);
-      }
-    });
-  }
+
   render() {
     return <span className="navbar-text">
-             {this.state.views} Views
+             {this.props.views} Views
            </span>
   }
 }
 
 VideoDetails.propTypes = {
-  videoId: React.PropTypes.string.isRequired
+  views: React.PropTypes.string.isRequired
 };
 
 window.VideoDetails = VideoDetails;
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,15 +1,52 @@
-var VideoPlayer = (props) => (
-  <div className="video-player">
-    <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src={"https://www.youtube.com/embed/" + props.video.id.videoId} allowFullScreen></iframe>
-    </div>
-    <div className="video-player-details">
-      <h3>{props.video.snippet.title}</h3>
-      <VideoDetails videoId={props.video.id.videoId} />
-      <div>{props.video.snippet.description}</div>
-    </div>
-  </div>
-);
+class VideoPlayer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      views: ''
+    }
+    this.request(props.video.id.videoId, this.fetchViews.bind(this));
+  };
+
+  fetchViews(view) {
+    this.setState({views: view});
+  }
+
+  request(id, callback) {
+    $.ajax({
+      type: 'GET',
+      data: {
+        id: id,
+        part: 'statistics',
+        key: YOUTUBE_API_KEY
+      },
+      url: 'https://www.googleapis.com/youtube/v3/videos',
+      success: function(data) {
+        console.log(data);
+        callback(data.items[0].statistics.viewCount);
+      },
+      error: function(data) {
+        console.log('Did not get statistics' + data);
+      }
+    });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.request(nextProps.video.id.videoId, this.fetchViews.bind(this));
+  }
+
+  render() {
+    return (<div className="video-player">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="embed-responsive-item" src={"https://www.youtube.com/embed/" + this.props.video.id.videoId} allowFullScreen></iframe>
+      </div>
+      <div className="video-player-details">
+        <h3>{this.props.video.snippet.title}</h3>
+        <VideoDetails views={this.state.views} />
+        <div>{this.props.video.snippet.description}</div>
+      </div>
+    </div>)
+  }
+};
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
